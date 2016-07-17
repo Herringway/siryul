d8303c765b0170e8bfab24b3a7c0f47d92b474dd
Cameron Ross <elpenguino@gmail.com>
2016-07-17 18:06:52 -0300

    yaml: include missing ForeachType import

--- source/siryul/yaml.d
+++ source/siryul/yaml.d
@@ -48,7 +48,7 @@ class YAMLSException : SerializeException {
 	}
 }
 private T fromYAML(T, BitFlags!DeSiryulize flags)(Node node) @trusted if (!isInfinite!T) {
-	import std.traits : isSomeString, isStaticArray, isAssociativeArray, isArray, isIntegral, isFloatingPoint, FieldNameTuple, KeyType, hasUDA, getUDAs, hasIndirections, ValueType, OriginalType, TemplateArgsOf, arity, Parameters;
+	import std.traits : isSomeString, isStaticArray, isAssociativeArray, isArray, isIntegral, isFloatingPoint, FieldNameTuple, KeyType, hasUDA, getUDAs, hasIndirections, ValueType, OriginalType, TemplateArgsOf, arity, Parameters, ForeachType;
 	import std.exception : enforce;
 	import std.datetime : SysTime, Date, DateTime, TimeOfDay;
 	import std.range : isOutputRange;
